28155622e8c2bc8a8f6e2bdc984fdcd11d28550c
Yandi Jin <55868579+Yandi1120@users.noreply.github.com>
2019-10-20 21:11:51 -0700

    Update test_create_dataframe.py

--- test_create_dataframe.py
+++ test_create_dataframe.py
@@ -1,6 +1,9 @@
 # Question 1
 import pandas as pd
-data = pd.read_csv('https://data.seattle.gov/api/views/tw7j-dfaw/rows.csv?accessType=DOWNLOAD')
+
+def read_url_and_create_csv(url_to_read):
+    data = pd.read_csv(url_to_read)
+    return data
 
 # Question 2
 import numpy as np
@@ -19,16 +22,19 @@ def test_create_dataframe(pd_df, col_list):
     result = True
     df_col_list = pd_df.columns.tolist()
     if pd_df[df_col_list[0]].count() + 1 >= 10:
-        if len(pd_df.columns) == len(col_list):
-            for col in col_list:
-                if col in df_col_list:
-                    if same_type(pd_df[col]):
-                        pass
-                    else: result = False
+        for df_col in df_col_list:
+            if df_col in col_list:
+                if same_type(df_col.tolist()):
+                    pass
                 else:
-                    result = False  
-        else:
-            result = False
+                    result = False
+            else:
+                result = False
+        for col in col_list:
+            if col in df_col_list:
+                pass
+            else:
+                result = False
     else:
         result = False
     return result
